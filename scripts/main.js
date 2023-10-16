d7e28b263b207e59c95051ee9b5819e2ac2909ac
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-10-15 21:41:49 -0300

    Adição dos botões (arrows) para trocar de slide no banner

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,23 @@ function nextImage() {
 
   document.getElementById("radio" + count).checked = true;
 }
+
+function prevImage() {
+  count--;
+
+  if (count < 1) {
+    count = 5;
+  }
+
+  document.getElementById("radio" + count).checked = true;
+}
+
+function nextImage() {
+  count++;
+
+  if (count > 5) {
+    count = 1;
+  }
+
+  document.getElementById("radio" + count).checked = true;
+}
